e1739fadcb9b3e76eba8d351132ebb57b63dc207
yasar <38501296+nowon48@users.noreply.github.com>
2022-08-02 20:30:47 +0300

    Create rebusclaim.sh

---
+++ rebusclaim.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+while true
+do
+	rebusd tx distribution withdraw-rewards $REBUS_VALOPER_ADDRESS --from=$WALLET --commission --chain-id=$REBUS_CHAIN_ID -y
+	sleep 10
+	amount=$(rebusd query bank balances $REBUS_WALLET_ADDRESS | grep -oP -m1 '(?<=")[^"]*')
+	rebusd tx staking delegate $REBUS_VALOPER_ADDRESS "${amount}arebus" --from=$WALLET --chain-id=$REBUS_CHAIN_ID --gas=auto -y
+	sleep 120
+done
+
