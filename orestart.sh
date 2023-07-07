cd74155680afa3dbebb01c06c02824142447c085
yasar <38501296+nowon48@users.noreply.github.com>
2023-07-07 15:54:16 +0300

    Create orestart.sh

---
+++ orestart.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+while true
+do
+	pkill geth
+  pkill beacon-chain
+  pkill validator
+  
+  echo "${Wait10Seconds}"
+  sleep 10
+  
+  "/root/testnet-auto-install-v3/opside-chain/start-geth.sh"
+	"/root/testnet-auto-install-v3/opside-chain/start-beaconChain.sh"
+	"/root/testnet-auto-install-v3/opside-chain/start-validator.sh"
+
+  sleep 3600
+done
